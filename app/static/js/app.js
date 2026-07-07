d514ceca2cdf21b5223598f2cc1b9df50cfe407b
Unique86 <cabraham1986@gmail.com>
2026-07-07 07:59:49 -0400

    Create reusable stock card and fix JavaScript scoping bug

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -19,14 +19,21 @@ searchButton.addEventListener("click", async function() {
     
     const data = await response.json();
     console.log("Data:", data);
+    
+
+
+    
     const changeClass = data.price_change >= 0 ? "positive" : "negative";
     stockResult.innerHTML = `
+    <div class="stock-card">
+
     <h3>${data.ticker}</h3>
 
     <p>Current Price: $${data.current_price}</p>
 
-    <p class="color: ${changeClass};">
-    <strong>Today's Change:</strong> $${data.price_change}
+    <p class="${changeClass}">
+        <strong>Today's Change:</strong>
+        $${data.price_change}
     </p>
 
     <p>Percent Change: ${data.percent_change}%</p>
@@ -38,5 +45,7 @@ searchButton.addEventListener("click", async function() {
     <p>Open: $${data.open_price}</p>
 
     <p>Previous Close: $${data.previous_close}</p>
+
+</div>
 `;
 });
