b7182c56b69f3b09077b4f91f4f10823ee65bd85
Unique86 <cabraham1986@gmail.com>
2026-07-06 21:55:44 -0400

    Connect frontend search to stock API

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,13 +4,39 @@ const searchButton = document.getElementById("searchButton")
 const tickerInput = document.getElementById("ticker")
 const stockResult = document.getElementById("stockResult");
 
+
+    
+
 searchButton.addEventListener("click", async function() {
+    console.log("Search button clicked!");
     const symbol = tickerInput.value;
+    console.log("Ticker:", symbol);
 
     stockResult.innerHTML = "<p>Loading stock data...</p>";
 
     const response = await fetch("/search?ticker=" + symbol);
+    console.log("Response received");
     
     const data = await response.json();
-    console.log(data);
+    console.log("Data:", data);
+    const changeClass = data.price_change >= 0 ? "positive" : "negative";
+    stockResult.innerHTML = `
+    <h3>${data.ticker}</h3>
+
+    <p>Current Price: $${data.current_price}</p>
+
+    <p class="color: ${changeClass};">
+    <strong>Today's Change:</strong> $${data.price_change}
+    </p>
+
+    <p>Percent Change: ${data.percent_change}%</p>
+
+    <p>Day High: $${data.day_high}</p>
+
+    <p>Day Low: $${data.day_low}</p>
+
+    <p>Open: $${data.open_price}</p>
+
+    <p>Previous Close: $${data.previous_close}</p>
+`;
 });
